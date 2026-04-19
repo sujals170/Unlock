5b91cff12e90abfac2df698b4477bf85721d7a81
Sujal Dhaduk <167689197+sujals170@users.noreply.github.com>
2026-04-19 14:58:21 +0530

    Add files via upload

---
+++ content.js
@@ -0,0 +1,28 @@
+var _0x0001 = (function() {
+  var _0x000a = [46,112,111,115,116,45,108,111,99,107,101,114].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x000b = [46,105,101,108,116,115,45,116,101,115,116,45,108,111,99,107,101,100,45,111,118,101,114,108,97,121].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x000c = [113,117,101,114,121,83,101,108,101,99,116,111,114,65,108,108].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x000d = [111,98,115,101,114,118,101].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x000e = [102,111,114,69,97,99,104].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x000f = [114,101,109,111,118,101].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x0010 = [99,104,105,108,100,76,105,115,116].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x0011 = [115,117,98,116,114,101,101].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+  var _0x0012 = [98,111,100,121].map(function(_0x0063){return String.fromCharCode(_0x0063);}).join('');
+
+  function _0x0014() {
+    var _0x001e = document[_0x000c](_0x000a);
+    _0x001e[_0x000e](function(_0x0032) { _0x0032[_0x000f](); });
+    var _0x001f = document[_0x000c](_0x000b);
+    _0x001f[_0x000e](function(_0x0033) { _0x0033[_0x000f](); });
+  }
+
+  _0x0014();
+
+  var _0x0028 = new MutationObserver(function() { _0x0014(); });
+  var _0x0029 = {};
+  _0x0029[_0x0010] = !![]; 
+  _0x0029[_0x0011] = !![];
+  _0x0028[_0x000d](document[_0x0012], _0x0029);
+
+  return {};
+}());
